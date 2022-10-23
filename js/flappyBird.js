1dd612d6b62d8adbaef8d978643c7e5cf4f9f4dc
Omar Gelis <omar.gelis.baena@gmail.com>
2022-10-22 19:56:39 -0500

    Funcionalidad del juego

--- js/flappyBird.js
+++ js/flappyBird.js
@@ -33,3 +33,57 @@ tuberiaSur.src = "img/tuberiaSur.png";
 
 var suelo = new Image()
 suelo.src = "img/suelo.png";
+//control
+function presionar() {
+    Personaje.y -= 25
+
+}
+//bucle
+setInterval(loop, 1000 / FPS)
+function loop() {
+    contexto.clearRect(0, 0, 300, 700);
+
+    //fondo
+    contexto.drawImage(background, 0, 0);
+
+    //suelo
+    contexto.drawImage(suelo, 0, contexto.canvas.height - suelo.height)
+
+
+    //personaje
+    contexto.drawImage(bird, Personaje.x, Personaje.y)
+
+    //tuberias
+    for (var i = 0; i < tuberias.length; i++) {
+        var constante = tuberiaNorte.height + 80
+        contexto.drawImage(tuberiaNorte, tuberias[i].x, tuberias[i].y)
+        contexto.drawImage(tuberiaSur, tuberias[i].x, tuberias[i].y + constante)
+        tuberias[i].x--
+        if (tuberias[i].y + tuberiaNorte.height < 80) {
+            tuberias[i].y = 0
+        }
+        if (tuberias[i].x == 95) {
+            tuberias.push({
+                x: contexto.canvas.width,
+                y: Math.floor(Math.random() * tuberiaNorte.height) - tuberiaNorte.height
+            })
+
+        }
+        //colisiones
+        if (Personaje.x + bird.width >= tuberias[i].x &&
+            Personaje.x <= tuberias[i].x + tuberiaNorte.width &&
+            (Personaje.y <= tuberias[i].y + tuberiaNorte.height ||
+                Personaje.y + bird.height >= tuberias[i].y + constante)
+            || Personaje.y + bird.height >= contexto.canvas.height - suelo.height) {
+            location.reload()
+        }
+
+        if (tuberias[i].x == Personaje.x) {
+            score++
+            punto.play()
+        }
+
+
+
+    }
+}
